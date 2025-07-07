c8d5b28d16fc820831025922af260cca960cd57b
kenziehh <ghaisani.nurani@gmail.com>
2025-07-08 00:08:06 +0700

    fix: update load_state_dict call to include weights_only parameter for resnet model

--- app/core/model.py
+++ app/core/model.py
@@ -8,7 +8,7 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForSequenceClassification
 def load_resnet_model(path: str):
     model = models.resnet50(pretrained=False)
     model.fc = nn.Linear(model.fc.in_features, 2)
-    model.load_state_dict(torch.load(path, map_location=torch.device("cpu")))
+    model.load_state_dict(torch.load(path, map_location=torch.device("cpu"), weights_only=False))
     model.eval()
     return model
 
